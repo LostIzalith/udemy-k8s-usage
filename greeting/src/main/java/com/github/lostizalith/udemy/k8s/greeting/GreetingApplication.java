e5fbe169501d4ea0300730c653eb8674f9fe0179
solairerove <hax2033@gmail.com>
2018-02-23 17:32:05 +0300

    feature: create multi module gradle project

---
+++ greeting/src/main/java/com/github/lostizalith/udemy/k8s/greeting/GreetingApplication.java
@@ -0,0 +1,12 @@
+package com.github.lostizalith.udemy.k8s.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(GreetingApplication.class, args);
+    }
+}
